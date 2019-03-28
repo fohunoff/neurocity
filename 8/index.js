b1483d4409f64662ce1e709fc90bbeb33dac8f8d
Иван Загайнов <fohunoff@gmail.com>
2019-03-28 14:56:49 +0300

    Eighth missions -- done

---
+++ 8/index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+(function () {
+  var button = document.querySelector('.button');
+  var boxes = document.querySelectorAll('.box');
+  var isOpen = false;
+
+  var PriceImages = {
+    car: 'img/car.png',
+    plant: 'img/plant.png',
+    tickets: 'img/tickets.png',
+    robot: 'img/robot.png',
+    duck: 'img/ducking.png',
+    sword: 'img/sword.png',
+  };
+
+  var Prices = Object.keys(PriceImages);
+
+  var superPrice = function (elem) {
+    elem.classList.add('super-price');
+  }
+
+  var openBox = function (box, index, priceImage) {
+    var cover = box.querySelector('.box__cover');
+    var price = box.querySelector('.box__inner');
+    var priceName = priceImage[index];
+
+    cover.classList.toggle('box__cover--opened');
+    price.classList.toggle('box__inner--show');
+    price.style.backgroundImage = 'url(' + PriceImages[priceName] + ')';
+    
+    if (priceName === 'car') {
+      superPrice(price);
+    }
+    
+    isOpen ? price.classList.remove('super-price') : null;
+  }
+  
+  button.addEventListener('click', function () {
+    if (!isOpen) {
+      Prices.sort(function (a, b) {
+        return Math.random() - 0.5;
+      });
+    };
+
+    for (var i = 0; i < 3; i++) {
+      openBox(boxes[i], i, Prices);
+    }
+
+    isOpen = !isOpen;
+
+
+  });
+})();
